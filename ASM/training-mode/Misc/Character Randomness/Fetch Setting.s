4969f2f09c6a9f7301abfce89f83fe494792f1c1
fluent <zSuckyLP@gmail.com>
2025-06-01 19:20:26 +0200

    added "Fetch Setting" macros, safeguard fetchsetting for normal modes

---
+++ ASM/training-mode/Misc/Character Randomness/Fetch Setting.s
@@ -0,0 +1,21 @@
+.include "../../../Globals.s"
+
+.macro CharRng_FetchSetting reg, default_branch
+load \reg, EventVars_Ptr
+lwz \reg, 0x0(\reg)
+lwz \reg, EventVars_EventGobj(\reg) # eventgobj
+cmpwi \reg, 0 # if eventgobj is null, we default to normal rng behavior
+beq \default_branch
+lwz \reg, 0x2c(\reg) # userdata
+lhz \reg, 0x2a(\reg) # setting, format: 0x00001200 where 1 is hmn and 2 is cpu
+.endm
+
+.macro CharRng_ExtractSetting reg, type
+.if \type == hmn
+rlwinm \reg, \reg, 0, 16, 19
+.elseif \type == cpu
+rlwinm \reg, \reg, 4, 16, 19
+.else
+"'hmn' or 'rng' expected as second argument"
+.endif
+.endm
--- ASM/training-mode/Misc/Character Randomness/Luigi Misfire.asm
+++ ASM/training-mode/Misc/Character Randomness/Luigi Misfire.asm
@@ -1,32 +1,25 @@
 # To be inserted at 80142ae0
 
-.include "../../../Globals.s"
+.include "Fetch Setting.s"
 
 # r3 = output, 0 is misfire, 1 is no misfire
 # r31 contains Fighter
 
 # TODO make macro for all randomness ASMs to easily retrieve and compare hmn/cpu rng setting
-
-load r6, EventVars_Ptr
-lwz r6, 0x0(r6)
-lwz r6, EventVars_EventGobj(r6) # eventgobj
-lwz r6, 0x2c(r6) # userdata
-
-lhz r6, 0x2a(r6) # setting, format: 0x00001200 where 1 is hmn and 2 is cpu
+CharRng_FetchSetting r6, RandomMisfire
 lbz r7, 0xc(r31) # player_id
 cmpwi r7, 0
 bne LoadCpuSetting
 
 LoadHmnSetting:
-  rlwinm r6, r6, 0, 16, 19
-  cmpwi r6, 0x1000
+  CharRng_ExtractSetting r6, hmn
   b BranchSettingCheck
 
 LoadCpuSetting:
-  rlwinm r6, r6, 0, 20, 23
-  cmpwi r6, 0x0100
+  CharRng_ExtractSetting r6, cpu
 
 BranchSettingCheck:
+  cmpwi r6, 0x1000
   blt+ RandomMisfire
   beq AlwaysMisfire
 
